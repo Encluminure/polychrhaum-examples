7238723dc86a4cf7da09e849912e1049a00ca5bf
JackDesBwa <jackdesbwa@gmail.com>
2014-10-17 17:50:45 +0200

    Example 1, simple skeleton

---
+++ 01_skeleton/01_skeleton.ino
@@ -0,0 +1,33 @@
+#include <PolychrHAUM.h>
+
+#ifdef BUILD_PC
+#define A0 0
+#define A1 1
+#endif
+
+#define PIN__BTN1          2
+#define PIN__BTN2          3
+#define PIN__POWER_BTN     4
+#define PIN__POWER_CMD     5
+#define PIN__POWER_STATUS  6
+#define PIN__LEDSTRIP_DATA 10
+#define PIN__POTAR_LIGHT   A0
+#define PIN__POTAR_SPEED   A1
+
+PolychrHAUM <150, PIN__LEDSTRIP_DATA> khroma;
+
+void animate() {
+}
+
+void setup() {
+	khroma.config_buttons(PIN__BTN1, PIN__BTN2);
+	khroma.config_power(PIN__POWER_CMD, PIN__POWER_STATUS, PIN__POWER_BTN);
+	khroma.config_light_ctrl(PIN__POTAR_LIGHT);
+	khroma.config_speed_ctrl(PIN__POTAR_SPEED);
+	khroma.config_animate(animate);
+	khroma.setup();
+}
+
+void loop() {
+	khroma.loop_step();
+}
